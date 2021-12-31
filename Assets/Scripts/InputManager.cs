40f7d6d49d2e8354e3aabb83ff0b3654b4c0f38b
Tilman Raendchen <tilman@medallyon.me>
2021-12-31 20:04:07 +0000

    Implement cycling through resolutions

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,7 @@ public class InputManager : MonoBehaviour
     {
         Actions.Enable();
     }
+
     private void OnDisable()
     {
         Actions.Disable();
@@ -41,8 +43,65 @@ public class InputManager : MonoBehaviour
         throw new System.NotImplementedException();
     }
 
-    private void CycleResolution(InputAction.CallbackContext obj)
+    protected List<Resolution> AvailableResolutions { get; } = new List<Resolution>
     {
-        throw new System.NotImplementedException();
+        new Resolution() { width = 1280, height = 720 },
+        new Resolution() { width = 1920, height = 1080 },
+        new Resolution() { width = 3860, height = 2160 }
+    };
+
+    protected Resolution CurrentResolution
+    {
+        get => AvailableResolutions[CurrentResolutionIndex];
+        set => Screen.SetResolution(value.width, value.height, Screen.fullScreen);
+    }
+
+    private int _currentResolutionIndex = 1;
+    protected int CurrentResolutionIndex
+    {
+        get => _currentResolutionIndex;
+        set => _currentResolutionIndex = Math.Min(AvailableResolutions.Count, Math.Max(0, value));
+    }
+
+    protected void CycleResolution(InputAction.CallbackContext context)
+    {
+        // Player wants to lower resolution
+        if (context.control.name.ToLower().Contains("left") || context.control.name.ToLower().Contains("q"))
+        {
+            // Resolution is already the lowest available
+            if (CurrentResolutionIndex == 0)
+                return;
+
+            // Check that monitor supports target resolution
+            if ((new List<Resolution>(Screen.resolutions)).FindIndex(res =>
+                res.width == AvailableResolutions[CurrentResolutionIndex - 1].width) == -1)
+            {
+                Debug.LogWarning($"This display does not support {AvailableResolutions[CurrentResolutionIndex - 1]}.");
+                return;
+            }
+
+            CurrentResolutionIndex--;
+        }
+
+        // Player wants to increase resolution
+        else if (context.control.name.ToLower().Contains("right") || context.control.name.ToLower().Contains("e"))
+        {
+            // Resolution is already the highest available
+            if (CurrentResolutionIndex == AvailableResolutions.Count - 1)
+                return;
+
+            // Check that monitor supports target resolution
+            if ((new List<Resolution>(Screen.resolutions)).FindIndex(res =>
+                res.width == AvailableResolutions[CurrentResolutionIndex + 1].width) == -1)
+            {
+                Debug.LogWarning($"This display does not support {AvailableResolutions[CurrentResolutionIndex + 1]}.");
+                return;
+            }
+
+            CurrentResolutionIndex++;
+        }
+
+        Debug.Log($"Setting Resolution to {AvailableResolutions[CurrentResolutionIndex]}");
+        CurrentResolution = AvailableResolutions[CurrentResolutionIndex];
     }
 }
