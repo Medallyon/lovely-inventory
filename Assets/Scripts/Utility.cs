cf692b9c34e1a53445148d9d686fb34e95c33cc6
Tilman Raendchen <tilman@medallyon.me>
2022-01-03 16:10:50 +0000

    Implement AudioSource extensions for use with EClips

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -1,10 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum AudioSourcePlayMode
+{
+    Random,
+    Average
+}
+
 public static class Utility
 {
+    public static void Play(this AudioSource source, SO_ExtendedClip clip)
+    {
+        source.pitch = clip.Pitch.Random;
+        source.PlayOneShot(clip.AudioClip, clip.Volume.Random);
+    }
+    public static void Play(this AudioSource source, SO_ExtendedClip clip, AudioSourcePlayMode mode)
+    {
+        switch (mode)
+        {
+            case AudioSourcePlayMode.Random:
+                source.Play(clip);
+                break;
+            case AudioSourcePlayMode.Average:
+                source.pitch = clip.Pitch.Average;
+                source.PlayOneShot(clip.AudioClip, clip.Volume.Average);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(mode), mode, null);
+        }
+    }
+
     public static void SetAlpha(this Graphic graphic, float alpha)
     {
         if (alpha > 1f)
